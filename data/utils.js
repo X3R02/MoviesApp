2185eed1775b0f7f50dfd49974ab63e22bd80053
X3R02 <x3r02mail@gmail.com>
2021-01-21 11:41:54 -0600

    Trabajando en la configuracion de la app

--- data/utils.js
+++ data/utils.js
@@ -1,1 +1,2 @@
-export const apiUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=9e6652e1580b3b6dcf46e8b7348b70c3';
+import { selector } from './config.json';
+export const apiUrl = `https://api.themoviedb.org/3/movie/${selector}?api_key=9e6652e1580b3b6dcf46e8b7348b70c3`;
--- screens/ConfigScreen.js
+++ screens/ConfigScreen.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import { Button, ScrollView, StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import configData from '../data/config.json';
 
 const ConfigScreen = () => {
+
+    const handleChangeSelector = () => {
+        if(configdata.lang === 'en-US') {
+            configData.lang = 'es-MX';
+        } else if (configData.lang === 'es-MX') {
+            configData.lang = 'en-US';
+        }
+        console.log(configData);
+    };
+
     return (
         <ScrollView>
             <View style={styles.container}>
-                <Button color="#bf42f5" title="Languaje"/>
-                <Button color="#bf42f5" title="Popular"/>
+                <TouchableOpacity
+                    style={styles.button}
+                >
+                    <Text style={styles.textButton}>
+                        Language
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={handleChangeSelector}
+                >
+                    <Text style={styles.textButton}>
+                        Popular
+                    </Text>
+                </TouchableOpacity>
             </View>
         </ScrollView>
     );
@@ -14,8 +38,18 @@ const ConfigScreen = () => {
 
 const styles = StyleSheet.create({
     container: {
-        padding: 30,
+        padding: 40,
     },
+    button: {
+        backgroundColor: '#bf42f5',
+        textAlign: 'center',
+        marginTop: 10,
+    },
+    textButton: {
+        fontSize: 20,
+        color: '#fff',
+        padding: 10,
+    }
 });
 
 export default ConfigScreen;
